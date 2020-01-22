a23dd887166788666ea260353638b545140897f6
DatDinh <datthanh071100@gmail.com>
2020-01-22 16:34:09 +0700

    Delete cat eat mouse

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 var Dog = require('./Dog');
 var Cat = require('./Cat');
-var Mouse = require('./Mouse');
 
 var dog = new Dog('Thành');
 var cat = new Cat();
-var mouse = new Mouse('Jerry');
 
-cat.eat(mouse);
 dog.eat(cat);
 
-console.log(dog, cat);
+console.log(dog);
